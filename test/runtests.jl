2d4c8b62cb6de0642bd6fab47f6ef124557c18bd
Elmar Zander <e.zander@tu-bs.de>
2016-08-15 17:15:22 +0200

    Added flag for throwing on errors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,14 @@
+using FactCheck
+FactCheck.clear_results()
+if !isdefined(:throw_on_failure)
+  throw_on_failure = true
+end
+
+module PkgTests
 include("test_multiindex.jl")
 include("test_distributions.jl")
+end
+
+if throw_on_failure
+  FactCheck.exitstatus()
+end
