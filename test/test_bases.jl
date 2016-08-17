0af853e8d78a8a8ad28bffcf810b33d31cb7d0bc
Elmar Zander <e.zander@tu-bs.de>
2016-08-17 14:41:01 +0200

    Fixed unittest for chebyshev polys

--- test/test_bases.jl
+++ test/test_bases.jl
@@ -1,35 +1,10 @@
 using Alea, FactCheck
 
-
-
 facts("orthpoly") do
   context("ChebyshevTPolynomials") do
     polys = ChebyshevTPolynomials()
-    @fact evaluate(polys, 4, 2) --> 1
+    cheb4(x) = [1, x, 2x.^2 - 1, 4x.^3 - 3x, 8x.^4 - 8x.^2 + 1]
+    @fact evaluate(polys, 4, 2) --> cheb4(2)
     @fact recurrence_coeff(polys, 3) --> (0, 2, 1)
   end
 end
-
-
-#=
-begin
-  basis = Bases.LaguerrePolynomials(1)
-  println(basis)
-  println(Bases.evaluate(basis, 3, 2))
-  plot(map(k->(x->Bases.evaluate(basis,k,x)[k+1]),0:7), 0, 5)
-end
-
-begin
-  basis = Bases.LegendrePolynomials()
-  println(basis)
-  println(Bases.evaluate(basis, 3, 2))
-  plot(map(k->(x->Bases.evaluate(basis,k,x)[k+1]),0:5), -1, 1)
-end
-
-begin
-  basis = Bases.ChebyshevTPolynomials()
-  println(basis)
-  println(Bases.evaluate(basis, 3, 2))
-  plot(map(k->(x->Bases.evaluate(basis,k,x)[k+1]),0:5), -1, 1)
-end
-=#
