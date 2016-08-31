89888a6cb170c5fed65fdfbcda58ef15b51e8c39
Elmar Zander <e.zander@tu-bs.de>
2016-08-31 13:38:46 +0200

    [wip] Testing email notifications on failed builds (2/should pass)

--- test/test_bases.jl
+++ test/test_bases.jl
@@ -5,7 +5,7 @@ facts("OrthPoly") do
     polys = ChebyshevTPolynomials()
     cheb4(x) = [1, x, 2x.^2 - 1, 4x.^3 - 3x, 8x.^4 - 8x.^2 + 1]
     @fact evaluate(polys, 4, 2) --> cheb4(2)
-    @fact recurrence_coeff(polys, 3) --> (0, 2, 1+6)
+    @fact recurrence_coeff(polys, 3) --> (0, 2, 1)
     @pending norm(polys, 3) --> 4
   end
 end
