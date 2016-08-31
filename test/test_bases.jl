cf81a208c5767a953f3af6424eb2396aabdf74ec
Elmar Zander <e.zander@tu-bs.de>
2016-08-31 12:55:13 +0200

    [wip] Testing email notifications on failed builds (1)

--- test/test_bases.jl
+++ test/test_bases.jl
@@ -1,10 +1,11 @@
 using Alea, FactCheck
 
-facts("orthpoly") do
+facts("OrthPoly") do
   context("ChebyshevTPolynomials") do
     polys = ChebyshevTPolynomials()
     cheb4(x) = [1, x, 2x.^2 - 1, 4x.^3 - 3x, 8x.^4 - 8x.^2 + 1]
     @fact evaluate(polys, 4, 2) --> cheb4(2)
-    @fact recurrence_coeff(polys, 3) --> (0, 2, 1)
+    @fact recurrence_coeff(polys, 3) --> (0, 2, 1+6)
+    @pending norm(polys, 3) --> 4
   end
 end
