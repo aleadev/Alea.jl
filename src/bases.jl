b002374c4208deb92bbc1eab053f5e4511baf110
Elmar Zander <e.zander@tu-bs.de>
2016-09-02 13:43:01 +0200

    [wip] on bases

--- src/bases.jl
+++ src/bases.jl
@@ -72,3 +72,63 @@ export ChebyshevUPolynomials
 recurrence_coeff(basis::ChebyshevUPolynomials, k::Integer) =
   0, 2, 1
 
+
+
+
+
+  function gauss_rule(basis::PolynomialSystem, n::Integer)
+  end
+
+
+  #module foobar
+  rc{T<:Integer}(k::Union{T,Array{T,1}}) =
+    zero(k), 1+zero(k), k
+  rc{T<:Integer}(k::AbstractArray{T,1}) = rc(Array{T,1}(k))
+  rc(1)
+  rc(3)
+  rc(1:3)
+
+  function recurrence_coeff(basis::PolynomialSystem, n::AbstractArray)
+    a1,b1,c1=recurrence_coeff(basis, n[1])
+    N=length(n)
+    (a,b,c)=(Array{typeof(a1),1}(N), Array{typeof(b1),1}(N), Array{typeof(c1),1}(N))
+    for i=1:N
+      (a[i], b[i], c[i]) = recurrence_coeff(basis, n[i])
+    end
+    return (a,b,c)
+  end
+
+
+  eltypes{T1}(::Type{Tuple{T1}}) = (T1,)
+  eltypes{T1,T2}(::Type{Tuple{T1,T2}}) = (T1, T2)
+  eltypes{T1,T2,T3}(::Type{Tuple{T1,T2,T3}}) = (T1, T2, T3)
+
+  function flip_tuple_array{T<:Tuple}(a::Array{T})
+    types = eltypes(T)
+    N = length(a)
+    b = [Array{t}(N) for t in types]
+    for i=1:length(types)
+      for j=1:N
+        b[i][j] = a[j][i]
+      end
+    end
+    return (b...)
+  end
+  function recurrence_coeff2(basis::PolynomialSystem, n::AbstractArray)
+    flip_tuple_array([recurrence_coeff(basis, k) for k in n])
+  end
+
+  a = [recurrence_coeff(LegendrePolynomials(), i) for i in 1:5]
+  @show a
+  @show typeof(a)
+  @show flip_tuple_array(a)
+
+  @show recurrence_coeff2(HermitePolynomials(),Array(0:3))
+  @show recurrence_coeff(LegendrePolynomials(),Array(0:3))
+  @show recurrence_coeff2(LegendrePolynomials(),Array(0:3))
+  @show recurrence_coeff(ChebyshevTPolynomials(),Array(0:5))
+  @show recurrence_coeff(ChebyshevUPolynomials(),Array(0:5))
+
+
+#end
+#
