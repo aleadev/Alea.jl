12078af3be1638d28089f1e9a65b80a64f7109f0
Elmar Zander <e.zander@tu-bs.de>
2016-09-02 16:27:38 +0200

    Rewrote the gauss tests with nicer algebraic expressions

--- test/test_bases.jl
+++ test/test_bases.jl
@@ -19,8 +19,11 @@ end
 facts("GaussIntegration") do
   @fact gauss_rule(HermitePolynomials(), 1) --> ([0.0], [1.0])
   @fact gauss_rule(HermitePolynomials(), 2) --> ([-1.0, 1.0], [0.5, 0.5])
-  @fact gauss_rule(HermitePolynomials(), 3)[1] --> roughly([-√3, 0, √3])
-  @fact gauss_rule(HermitePolynomials(), 3)[2] --> roughly([1/6, 4/6, 1/6])
-
   @fact gauss_rule(HermitePolynomials(), 3) --> roughly(([-√3, 0, √3], [1/6, 4/6, 1/6]))
+
+  @fact gauss_rule(LegendrePolynomials(), 2) --> ([-√(1/3), √(1/3)], [0.5, 0.5])
+  @fact gauss_rule(LegendrePolynomials(), 3) --> roughly(([-√(3/5), 0, √(3/5)], [5, 8, 5]/18))
+
+  @fact gauss_rule(LaguerrePolynomials(0.0), 2) --> roughly(([2-√2, 2+√2],
+                                                           [1/2+1/√8, 1/2-1/√8]))
 end
